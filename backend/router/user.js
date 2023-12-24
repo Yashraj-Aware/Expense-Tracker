b59dc6d042bd4659118781ab56174252b76538a5
Yashraj Aware <yashrajaware2003@gmail.com>
2023-12-24 10:34:56 +0530

    user authentication setup

---
+++ backend/router/user.js
@@ -0,0 +1,12 @@
+const { loginUser, signupUser } = require('../controller/UserController')
+const express = require("express")
+
+const router = express.Router()
+
+// login route
+router.post("/login", loginUser)
+
+// signup route
+router.post("/signup", signupUser)
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const incomeRoutes = require("./router/transactions");
+const userRoutes = require("./router/user");
 
 //express app
 const app = express();
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
 
 //routes
 app.use("/api/income", incomeRoutes);
+app.use("/api/user", userRoutes);
 
 // connection to db
 mongoose

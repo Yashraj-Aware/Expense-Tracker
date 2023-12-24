8def0811440c015f7e97788d73d9cabf2e827d35
Yashraj Aware <yashrajaware2003@gmail.com>
2023-12-24 21:05:54 +0530

    created auth context for authentication

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { IncomesContextProvider } from "./context/IncomeContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <IncomesContextProvider>
-      <App />
-    </IncomesContextProvider>
+    <AuthContextProvider>
+      <IncomesContextProvider>
+        <App />
+      </IncomesContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
